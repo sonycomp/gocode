c05f9095b5f761e636d06acf259f9a73c1dd1178
sonycomp@rediffmail.com <sonycomp@rediffmail.com>
2019-07-11 10:05:17 +0530

    added ext pkg

--- strutils/main.go
+++ strutils/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"math"
 
+	"github.com/bradtraversy/go_crash_course/tree/master/03_packages/strutil"
+)
+
+func main() {
+	fmt.Println(math.Sqrt(49))
+	fmt.Println(strutil.Reverse("rahul"))
 }
